4a095c2e452bceb069014191300e933b71d6ea55
198176 <adriantomczak36@gmail.com>
2018-01-13 17:25:40 +0100

    Test z różnymi wartościami zbioru

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -11,7 +11,7 @@ public class SimilarityFinderTest {
     private SimilarityFinder finder;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         finder = new SimilarityFinder(new MockSequenceSearcher());
     }
 
@@ -20,5 +20,12 @@ public class SimilarityFinderTest {
         int[] tab1 = {1, 2, 3, 4};
         int[] tab2 = {1, 2, 3, 4};
         assertThat(finder.calculateJackardSimilarity(tab1, tab2), is(1.0));
+    }
+
+    @Test
+    public void testDifferentValues() {
+        int[] tab1 = {1, 2, 3, 4};
+        int[] tab2 = {5, 6, 7, 8};
+        assertThat(finder.calculateJackardSimilarity(tab1, tab2), is(0.0));
     }
 }
