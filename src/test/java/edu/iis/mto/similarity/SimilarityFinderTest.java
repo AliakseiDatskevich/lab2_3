35e890f93a306b8c804c6e5cb98e961afcdbc631
Kaminski.Lukasz <198151@edu.p.lodz.pl>
2018-01-16 00:33:55 +0100

    [lab2_3]Similarity Finder tests (case - sqeuence is empty)

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,17 +1,43 @@
 package edu.iis.mto.similarity;
 
+import static org.junit.Assert.assertThat;
+
+import org.hamcrest.Matchers;
 import org.junit.Test;
+
+import dublers.SequenceSearcherDubler;
 
 public class SimilarityFinderTest {
 
     @Test
     public void calculateJackardSimilarityBothSequencesAreEmpty() {
-        ;
+        int[] fisrtSequence = {};
+        int[] secondSequence = {};
+        SimilarityFinder objectUnderTest = new SimilarityFinder(new SequenceSearcherDubler());
+        double expectedJackardSimilarity = 1.;
+        double actualJackardSimilarity = objectUnderTest.calculateJackardSimilarity(fisrtSequence, secondSequence);
+        double delta = Math.abs(actualJackardSimilarity - expectedJackardSimilarity);
+        double epsilon = 0.00000001;
+        boolean actualOutput = delta < epsilon;
+        boolean expectedOutput = true;
+
+        assertThat(expectedOutput, Matchers.equalTo(actualOutput));
+
     }
 
     @Test
     public void calculateJackardSimilaritySecondSequencesIsEmpty() {
-        ;
+        int[] fisrtSequence = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] secondSequence = {};
+        SimilarityFinder objectUnderTest = new SimilarityFinder(new SequenceSearcherDubler());
+        double expectedJackardSimilarity = 0.;
+        double actualJackardSimilarity = objectUnderTest.calculateJackardSimilarity(fisrtSequence, secondSequence);
+        double delta = Math.abs(actualJackardSimilarity - expectedJackardSimilarity);
+        double epsilon = 0.00000001;
+        boolean actualOutput = delta < epsilon;
+        boolean expectedOutput = true;
+
+        assertThat(expectedOutput, Matchers.equalTo(actualOutput));
     }
 
     @Test
