97621b09e9fd0bd01e4eef54e52d3ee71d106174
Chojnacki.Piotr <198144@edu.p.lodz.pl>
2018-01-18 01:28:05 +0100

    test with two complete different sequence

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -20,12 +20,23 @@ public class SimilarityFinderTest {
     }
 
     @Test
-    public void calculateJackardSimilarityBothSeqSame() {
+    public void calculateJackardSimilarityBothSeqSame(){
         SimilarityFinder similarityFinder = new SimilarityFinder(new SequenceSearcherDubler());
         int[] seq1 = {1, 2, 3, 4};
         int[] seq2 = {1, 2, 3, 4};
         double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
         double expected = 1.0d;
+
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void calculateJackardSimilarityBothSeqDifferent(){
+        SimilarityFinder similarityFinder = new SimilarityFinder(new SequenceSearcherDubler());
+        int[] seq1 = {1, 2, 3, 4};
+        int[] seq2 = {5, 7, 7, 10, 11};
+        double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+        double expected = 0.0d;
 
         assertThat(result, is(expected));
     }
