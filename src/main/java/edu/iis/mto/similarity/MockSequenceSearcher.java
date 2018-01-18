5abdc336e8c16be3eee8860f14e7e0ffae9e5967
Radoslaw Subczynski <radoslaw.subczynski@abastra.com>
2018-01-19 00:51:47 +0100

    dodanie klasy zapamiętującej wywowałania interfejsu

--- src/main/java/edu/iis/mto/similarity/MockSequenceSearcher.java
+++ src/main/java/edu/iis/mto/similarity/MockSequenceSearcher.java
@@ -4,10 +4,11 @@ import edu.iis.mto.search.SearchResult;
 import edu.iis.mto.search.SequenceSearcher;
 
 public class MockSequenceSearcher implements SequenceSearcher {
+    private Call callHierarchy ;
     private int callCounter = 0;
-
     public SearchResult search(int i, int[] ints) {
         int pos = 0;
+        callHierarchy = new Call(i, ints);
         callCounter++;
         for (int j : ints) {
             if (i == j) {
@@ -20,5 +21,22 @@ public class MockSequenceSearcher implements SequenceSearcher {
 
     public int getCallCounter() {
         return callCounter;
+    }
+    public int getParam(){
+        return callHierarchy.param;
+    }
+
+    public int[] getSeq(){
+        return callHierarchy.sequence;
+    }
+
+    private class Call{
+        int param = -1;
+        int[] sequence;
+        public Call(int param, int[] sequence) {
+            this.param = param;
+            this.sequence = sequence;
+        }
+
     }
 }
