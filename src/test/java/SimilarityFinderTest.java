7165bb1095fd10a186a796df84b6ae7da5ac6896
Radoslaw Subczynski <radoslaw.subczynski@abastra.com>
2018-01-18 23:25:02 +0100

    dodanie testu funkcji calculateJackardSimilarity dla pustych sekwencji

--- src/test/java/SimilarityFinderTest.java
+++ src/test/java/SimilarityFinderTest.java
@@ -18,4 +18,16 @@ public class SimilarityFinderTest {
 
         assertThat(result, is(expected));
     }
+
+    @Test
+    public void testCalculateJackardEmptySeq() throws Exception {
+        SimilarityFinder similarityFinder = new SimilarityFinder(new MockSequenceSearcher());
+        int[] seq1 = new int[] {};
+        int[] seq2 = new int[] {};
+        double expected = (double) 1;
+
+        double result = similarityFinder.calculateJackardSimilarity(seq1,seq2);
+
+        assertThat(result, is(expected));
+    }
 }
