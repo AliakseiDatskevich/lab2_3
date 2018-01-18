b680f70686f3d7c8c5d608caa06044bb098ac954
Radoslaw Subczynski <radoslaw.subczynski@abastra.com>
2018-01-18 22:47:56 +0100

    dodanie testu funkcji calculateJackardSimilarity dla ciągów o różnej długości + przeniesienie plików w odpowienie miejsce

---
+++ src/test/java/SimilarityFinderTest.java
@@ -0,0 +1,21 @@
+import edu.iis.mto.similarity.MockSequenceSearcher;
+import edu.iis.mto.similarity.SimilarityFinder;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class SimilarityFinderTest {
+
+    @Test
+    public void testCalculateJackardSimilarityAnotherSizeSeq() throws Exception {
+        SimilarityFinder similarityFinder = new SimilarityFinder(new MockSequenceSearcher());
+        int[] seq1 = new int[] {1,2,3,4,5};
+        int[] seq2 = new int[] {1,2,3};
+        double expected = (double) 3/5;
+
+        double result = similarityFinder.calculateJackardSimilarity(seq1,seq2);
+
+        assertThat(result, is(expected));
+    }
+}
