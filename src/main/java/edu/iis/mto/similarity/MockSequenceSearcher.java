d9aefa98968ebe873deeac2acc5e427446b85c25
Radoslaw Subczynski <radoslaw.subczynski@abastra.com>
2018-01-19 21:21:30 +0100

    Poprawka do merge request

--- src/main/java/edu/iis/mto/similarity/MockSequenceSearcher.java
+++ src/main/java/edu/iis/mto/similarity/MockSequenceSearcher.java
@@ -9,16 +9,13 @@ public class MockSequenceSearcher implements SequenceSearcher {
     private int callCounter = 0;
 
     public SearchResult search(int i, int[] ints) {
-        int pos = 0;
-        callHierarchy = new Call(i, ints);
-        callCounter++;
-        for (int j : ints) {
-            if (i == j) {
-                return new MockSearchResult(true, pos);
-            }
-            pos++;
-        }
-        return new MockSearchResult(false, -1);
+        SearchContainer searchContainer = new SearchContainer();
+
+        searchContainer.key = i;
+        searchContainer.seq = ints;
+        searchContainer.result = calcResult(i, ints);
+
+        return searchContainer.result;
     }
 
     public int getCallCounter() {
@@ -27,6 +24,13 @@ public class MockSequenceSearcher implements SequenceSearcher {
 
     public int getParam() {
         return callHierarchy.param;
+    }
+
+    public static class SearchContainer {
+
+        public int key;
+        public int[] seq;
+        public MockSearchResult result;
     }
 
     public int[] getSeq() {
@@ -43,5 +47,18 @@ public class MockSequenceSearcher implements SequenceSearcher {
             this.sequence = sequence;
         }
 
+    }
+
+    private MockSearchResult calcResult(int i, int[] ints) {
+        int pos = 0;
+        callHierarchy = new Call(i, ints);
+        callCounter++;
+        for (int j : ints) {
+            if (i == j) {
+                return new MockSearchResult(true, pos);
+            }
+            pos++;
+        }
+        return new MockSearchResult(false, -1);
     }
 }
