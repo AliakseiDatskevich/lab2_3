a580c1d45d0cf64db04f437a3b94fc33b225206b
Strek.Andrzej <198173.edu.p.lodz.pl>
2018-01-20 23:32:20 +0100

    refaktoryzacja dublerów i dodanie testu similarityTwoDifferentSentencesTest

--- src/main/java/edu/iis/mto/similarity/SequenceSearcherDubler.java
+++ src/main/java/edu/iis/mto/similarity/SequenceSearcherDubler.java
@@ -1,24 +1,37 @@
 package edu.iis.mto.similarity;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 
 import edu.iis.mto.search.SearchResult;
 import edu.iis.mto.search.SequenceSearcher;
 
 public class SequenceSearcherDubler implements SequenceSearcher {
 
+    private int counter = 0;
+    private List<SearchResult> resultList = new ArrayList<SearchResult>();
+    private List<Integer> keyList = new ArrayList<Integer>();
+
     public SearchResult search(int key, int[] seq) {
-        boolean found = false;
-        int position = -1;
-        for (int s : seq) {
-            if (key == s) {
-                found = true;
-                position = new ArrayList<int[]>(Arrays.asList(seq)).indexOf(s);
-                break;
-            }
-        }
-        System.out.println(found);
-        return new SearchResultDubler(found, position);
+        keyList.add(key);
+        SearchResult result = resultList.get(counter);
+        counter++;
+        return result;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setSearchResultList(List<SearchResult> resultList) {
+        this.resultList = resultList;
+    }
+
+    public void addSearchResultToList(SearchResult searchResult) {
+        resultList.add(searchResult);
+    }
+
+    public List<Integer> getKeyList() {
+        return keyList;
     }
 }
--- src/test/java/lab2_3/TestSimilarityFinder.java
+++ src/test/java/lab2_3/TestSimilarityFinder.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertThat;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 
+import edu.iis.mto.similarity.SearchResultDubler;
 import edu.iis.mto.similarity.SequenceSearcherDubler;
 import edu.iis.mto.similarity.SimilarityFinder;
 
@@ -12,12 +13,45 @@ public class TestSimilarityFinder {
 
     @Test
     public void similarityTwoSameSequencesTest() {
+        SequenceSearcherDubler searcherDubler = new SequenceSearcherDubler();
+        searcherDubler.addSearchResultToList(new SearchResultDubler(true, 0));
+        searcherDubler.addSearchResultToList(new SearchResultDubler(true, 1));
+        searcherDubler.addSearchResultToList(new SearchResultDubler(true, 2));
+
+        SimilarityFinder similarityFinder = new SimilarityFinder(searcherDubler);
+        int[] firstSequence = {10, 11, 2};
+        int[] secondSequence = {10, 11, 2};
+        double result = similarityFinder.calculateJackardSimilarity(firstSequence, secondSequence);
+        double expected = 1.0d;
+
+        assertThat(result, Matchers.is(expected));
+    }
+
+    @Test
+    public void similarityTwoEmptySequencesTest() {
         SimilarityFinder similarityFinder = new SimilarityFinder(new SequenceSearcherDubler());
-        int[] firstSeqence = {10, 11, 2};
-        int[] secondSeqeuence = {10, 11, 2};
+        int[] firstSeqence = {};
+        int[] secondSeqeuence = {};
         double result = similarityFinder.calculateJackardSimilarity(firstSeqence, secondSeqeuence);
         double expected = 1.0d;
 
         assertThat(result, Matchers.is(expected));
     }
+
+    @Test
+    public void similarityTwoDifferentSentencesTest() {
+        SequenceSearcherDubler searcherDubler = new SequenceSearcherDubler();
+        searcherDubler.addSearchResultToList(new SearchResultDubler(false));
+        searcherDubler.addSearchResultToList(new SearchResultDubler(false));
+        searcherDubler.addSearchResultToList(new SearchResultDubler(false));
+
+        SimilarityFinder similarityFinder = new SimilarityFinder(searcherDubler);
+        int[] firstSeqence = {10, 11, 2};
+        int[] secondSeqence = {10, 11, 12};
+        double result = similarityFinder.calculateJackardSimilarity(firstSeqence, secondSeqence);
+        double expected = 0.0d;
+
+        assertThat(result, Matchers.is(expected));
+    }
+
 }
