8b171add093bcf532844d9993414e33e29e74d62
jankowski.lukasz <191867@edu.p.lodz.pl>
2018-01-21 16:27:37 +0100

    Dodanie testu similarSequenceReturnValueBeetwenZeroAndOneInJaccardaIndex

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -3,6 +3,9 @@ package edu.iis.mto.similarity;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
@@ -26,5 +29,14 @@ public class SimilarityFinderTest {
         int[] seq2 = {117, 1117, 1127, 1137};
         assertThat(similarityFinder.calculateJackardSimilarity(seq1, seq2), is(0.0));
     }
+
+    @Test
+    public void similarSequenceReturnValueBeetwenZeroAndOneInJaccardaIndex() {
+        SimilarityFinder similarityFinder = new SimilarityFinder(new SequenceSeracherImpl());
+        int[] seq1 = {7, 17, 27, 37};
+        int[] seq2 = {7, 1117, 1127, 1137};
+        assertThat(similarityFinder.calculateJackardSimilarity(seq1, seq2), allOf(greaterThan(0.0), lessThan(1.0)));
+    }
+
 
 }
