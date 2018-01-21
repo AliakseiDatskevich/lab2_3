137140a24a776fde70228a2e6104e3b5288d4134
jankowski.lukasz <191867@edu.p.lodz.pl>
2018-01-21 15:45:23 +0100

    Dodanie testu differentSequenceReturnZeroInJaccardaIndex

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -19,6 +19,12 @@ public class SimilarityFinderTest {
         assertThat(similarityFinder.calculateJackardSimilarity(seq1, seq2), is(1.0));
     }
 
-
+    @Test
+    public void differentSequenceReturnZeroInJaccardaIndex() {
+        SimilarityFinder similarityFinder = new SimilarityFinder(new SequenceSeracherImpl());
+        int[] seq1 = {7, 17, 27, 37};
+        int[] seq2 = {117, 1117, 1127, 1137};
+        assertThat(similarityFinder.calculateJackardSimilarity(seq1, seq2), is(0.0));
+    }
 
 }
