f18c1ab69f674f71731f28b7ab1b5d0221231a5e
jankowski.lukasz <191867@edu.p.lodz.pl>
2018-01-21 15:39:19 +0100

    Dodanie testu sameSequenceReturnOneInJaccardaIndex

---
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -0,0 +1,22 @@
+package edu.iis.mto.similarity;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Lukasz on 2018-01-21.
+ */
+public class SimilarityFinderTest {
+
+    @Test
+    public void sameSequenceReturnOneInJaccardaIndex() {
+        SimilarityFinder similarityFinder = new SimilarityFinder(new SequenceSeracherImpl());
+        int[] seq1 = {7, 17, 27, 37};
+        int[] seq2 = {7, 17, 27, 37};
+        assertThat(similarityFinder.calculateJackardSimilarity(seq1, seq2), is(1.0));
+    }
+
+}
