5ad24c738f24dbf12aa1246adf7aa977eda8f1ec
smoczynskaj <smoczynska.justyna@gmail.com>
2018-01-25 20:09:28 +0100

    Test dla identycznych zbiorów

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,5 +1,6 @@
 package edu.iis.mto.similarity;
 
+import edu.iis.mto.search.SequenceSearcher;
 import org.junit.Test;
 
 import java.math.BigDecimal;
@@ -17,6 +18,20 @@ public class SimilarityFinderTest {
         int[] seq2 = {};
 
         SimilarityFinder similarityFinder = new SimilarityFinder(new SequenceSearcherDubler());
+
+        double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+
+        assertEquals(BigDecimal.valueOf(result), BigDecimal.valueOf(1.0d));
+    }
+
+    @Test
+    public void areSequencesIdentical() throws Exception {
+        int[] seq1 = {1, 0, 3, 22, 7, 2, 99};
+        int[] seq2 = {22, 1, 7, 99, 3, 2, 0};
+
+        SequenceSearcher dubler = new SequenceSearcherDubler(true, true, true, true, true, true, true);
+
+        SimilarityFinder similarityFinder = new SimilarityFinder(dubler);
 
         double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
 
