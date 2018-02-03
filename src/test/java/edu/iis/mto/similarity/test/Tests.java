1b8d3384b8fd4de9c133724f61ce909433a9b723
Aleksander Bella <bella.aleksander@gmail.com>
2018-02-03 17:09:05 +0100

    Added similarity test for both sequences empty

--- src/test/java/edu/iis/mto/similarity/test/Tests.java
+++ src/test/java/edu/iis/mto/similarity/test/Tests.java
@@ -1,12 +1,24 @@
 package edu.iis.mto.similarity.test;
 
+import static org.junit.Assert.assertThat;
+
+import org.hamcrest.Matchers;
 import org.junit.Test;
+
+import edu.iis.mto.similarity.SequenceSearcherDubler;
+import edu.iis.mto.similarity.SimilarityFinder;
 
 public class Tests {
 
     @Test
-    public void test() {
+    public void similarityBothSequenceEmpty() {
+        SimilarityFinder similarityFinder = new SimilarityFinder(new SequenceSearcherDubler());
+        int[] seq1 = {};
+        int[] seq2 = {};
 
+        double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+
+        assertThat(result, Matchers.is(1.0));
     }
 
 }
