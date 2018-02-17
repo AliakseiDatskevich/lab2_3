6897b25fefa468c6e8754a919e26941eb1ae9333
Aliaksei Datskevich <187672@edu.p.lodz.pl>
2018-02-17 20:25:35 +0100

    Added in Test.java 3 tests: another sequance and 2 same shorter tests

--- src/test/java/lab2_3/Test.java
+++ src/test/java/lab2_3/Test.java
@@ -28,4 +28,37 @@ public class Test {
         System.out.println(sim);
         assertEquals(sim == 0.0, true);
     }
+
+    @org.junit.Test
+    public void shorterSequenceTest1() {
+        DublerSequenceResult searcherDubler = new DublerSequenceResult();
+        SimilarityFinder similarityFinder = new SimilarityFinder(searcherDubler);
+        int seq1[] = {2, 345, 34, 3462};
+        int seq2[] = {2, 345, 34, 3462, 141};
+        double sim = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+        System.out.println(sim);
+        assertEquals(sim == 0.8, true);
+    }
+
+    @org.junit.Test
+    public void shorterSequenceTest2() {
+        DublerSequenceResult searcherDubler = new DublerSequenceResult();
+        SimilarityFinder similarityFinder = new SimilarityFinder(searcherDubler);
+        int seq1[] = {345, 34, 3462, 141};
+        int seq2[] = {2, 345, 34, 3462, 141};
+        double sim = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+        System.out.println(sim);
+        assertEquals(sim == 0.8, true);
+    }
+
+    @org.junit.Test
+    public void anotherSequenceTest() {
+        DublerSequenceResult searcherDubler = new DublerSequenceResult();
+        SimilarityFinder similarityFinder = new SimilarityFinder(searcherDubler);
+        int seq1[] = {2, 345, 34, 3462, 141};
+        int seq2[] = {34, 141, 2, 345, 3462};
+        double sim = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+        System.out.println(sim);
+        assertEquals(sim == 1.0, true);
+    }
 }
