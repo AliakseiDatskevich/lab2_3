2d2d415e156a4eb49d5382695f98c5790daa855d
AliakseiDatskevich <187672@edu.p.lodz.pl>
2018-02-19 10:48:28 +0100

    Changes in DublerSequenceResult

--- src/main/java/edu/iis/mto/similarity/DublerSequenceResult.java
+++ src/main/java/edu/iis/mto/similarity/DublerSequenceResult.java
@@ -1,41 +1,16 @@
 package edu.iis.mto.similarity;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import edu.iis.mto.search.SearchResult;
 import edu.iis.mto.search.SequenceSearcher;
 
 public class DublerSequenceResult implements SequenceSearcher {
 
-    public List<SearchCall> searchCallHistory = new ArrayList<SearchCall>();
-
-    public static class SearchCall {
-
-        public int key;
-        public int[] seq;
-        public DublerSearchResult result;
-    }
-
-    public SearchResult search(int key, int[] seq) {
-        SearchCall call = new SearchCall();
-        call.key = key;
-        call.seq = Arrays.copyOf(seq, seq.length);
-        call.result = normalSearch(key, seq);
-        searchCallHistory.add(call);
-        return call.result;
-    }
-
-    private DublerSearchResult normalSearch(int key, int[] seq) {
-        boolean f = false;
-        int p = -1;
-        for (int i = 0; i < seq.length; ++i) {
-            if (key == seq[i]) {
-                f = true;
-                p = i;
-            }
-        }
-        return new DublerSearchResult(f, p);
-    }
+	public SearchResult search(int key, int[] sequence) {
+		for (int i = 0; i < sequence.length; i++) {
+			if (sequence[i] == key) {
+				return new DublerSearchResult(true, i);
+			}
+		}
+		return new DublerSearchResult(false, -1);
+	}
 }
